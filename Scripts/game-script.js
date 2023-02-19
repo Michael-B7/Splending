845ac999f7405684e0b675802df7ccdd79878978
Emiliano C <emiliano.cereceres@student.cart.org>
2023-02-18 16:47:12 -0800

    gem hover effects

--- Scripts/game-script.js
+++ Scripts/game-script.js
@@ -2,3 +2,20 @@
 window.addEventListener("resize", function() {
     console.log(window.outerHeight)
 })
+
+console.log(location.href);
+
+console.log(document.getElementsByClassName("card"))
+const cards = document.getElementsByClassName("card");
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function() {
+        console.log("yipeeee", this)
+    })
+    cards[i].addEventListener('mouseover', function(event) {
+        console.log("wowowowowo")
+        console.log(event.target)
+        this.querySelector('.card-options').style.display = 'none';
+        this.querySelector('.card-content').style.opacity = 1;
+    });
+}
